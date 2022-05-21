ce0b9e839895aa0e90cef71e21a60ab6c6e69447
sujon-chambugong <sujonchambugong@gmail.com>
2022-05-22 00:55:25 +0600

    some pages, navbar, footer, fixed some issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import About from './components/Pages/About';
+import Blogs from './components/Pages/Blogs';
+import Contact from './components/Pages/Contact';
+import Home from './components/Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar></Navbar>
+
+      <Routes>
+
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Pages/Contact.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div class="card card-side bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://api.lorem.space/image/movie?w=200&h=280"
+          alt="Movie"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Watch</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
